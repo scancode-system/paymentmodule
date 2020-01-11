f7ebec3d928ca4589f8a6418fc953c6e8d53fdea
Your Name <you@example.com>
2020-01-11 14:33:06 -0300

    .

--- Services/ImportService.php
+++ Services/ImportService.php
@@ -4,12 +4,17 @@ namespace Modules\Payment\Services;
 
 use Modules\Payment\Imports\PaymentsImport;
 use Maatwebsite\Excel\Facades\Excel;
+use Illuminate\Support\Facades\Storage;
 
 class ImportService {
 
-    public function import($path)
-    {
-    	Excel::import(new PaymentsImport, $path);
-    }
+	public function import($path)
+	{
+		if(Storage::exists($path)){
+			Excel::import(new PaymentsImport, $path);
+		}
+	}
 
 }
+
+
